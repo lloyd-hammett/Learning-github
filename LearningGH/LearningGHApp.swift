390112ad094fd5c7464c5b2b0cdf8aafda184d44
Lloyd Hammett <lloyd.hammett@outlook.com>
2023-02-01 08:39:12 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Lloyd  on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
